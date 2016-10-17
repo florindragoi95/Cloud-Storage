1bfe78341d7298e3f0a8b47315c3f5246aa64710
florindragoi95 <florindragoi95@gmail.com>
2016-10-17 20:13:38 +0300

    base url fixed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/cloudstorage/index.php';
+$config['base_url'] = 'http://localhost:8080/cloud-storage/index.php';
 
 /*
 |--------------------------------------------------------------------------
